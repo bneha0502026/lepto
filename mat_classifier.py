7cd51797dff23c3b1a087fac7c9a1261ba092c8e
nehaadithya01-hub <nehabalamurugan05@gmail.com>
2026-05-14 23:34:52 +0530

    fixed streamlit model

--- mat_classifier.py
+++ mat_classifier.py
@@ -1,53 +1,43 @@
 import torch
 import torch.nn as nn
 
-from torchvision import models, transforms
+from torchvision import transforms
+from torchvision.models import (
+    efficientnet_v2_s,
+    EfficientNet_V2_S_Weights
+)
 
 from PIL import Image
 
-
-# =====================================================
+# =========================================================
 # IMAGE TRANSFORM
-# =====================================================
+# =========================================================
 
 transform = transforms.Compose([
-
-    transforms.Resize((224,224)),
-
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
-
     transforms.Normalize(
-        [0.485, 0.456, 0.406],
-        [0.229, 0.224, 0.225]
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
     )
 ])
 
-
-# =====================================================
+# =========================================================
 # LOAD MODEL
-# =====================================================
+# =========================================================
 
 def load_model(idx_to_class):
 
-    model = models.efficientnet_v2_s(pretrained=False)
-
-    num_features = model.classifier[1].in_features
+    model = efficientnet_v2_s(
+        weights=EfficientNet_V2_S_Weights.DEFAULT
+    )
 
     model.classifier = nn.Sequential(
-
-        nn.Dropout(0.5),
-
-        nn.Linear(num_features, 512),
-
-        nn.ReLU(),
-
         nn.Dropout(0.3),
-
-        nn.Linear(512, len(idx_to_class))
+        nn.Linear(1280, len(idx_to_class))
     )
 
     model.load_state_dict(
-
         torch.load(
             "lepto_model.pth",
             map_location=torch.device("cpu")
@@ -58,10 +48,9 @@ def load_model(idx_to_class):
 
     return model
 
-
-# =====================================================
+# =========================================================
 # PREDICTION
-# =====================================================
+# =========================================================
 
 def predict(model, image_path, idx_to_class):
 
@@ -73,19 +62,11 @@ def predict(model, image_path, idx_to_class):
 
         outputs = model(img_tensor)
 
-        probabilities = torch.softmax(
-            outputs,
-            dim=1
-        )
+        probs = torch.softmax(outputs, dim=1)
 
-        confidence, predicted = torch.max(
-            probabilities,
-            1
-        )
+        confidence, predicted = torch.max(probs, 1)
 
-    class_name = idx_to_class[
-        predicted.item()
-    ]
+    class_name = idx_to_class[predicted.item()]
 
     return (
         class_name,
