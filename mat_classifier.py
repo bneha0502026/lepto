e3b421ed40d4bf91edf2fa4ec4dde845fc2dc61b
nehaadithya01-hub <nehabalamurugan05@gmail.com>
2026-05-15 00:09:20 +0530

    Update mat_classifier.py

--- mat_classifier.py
+++ mat_classifier.py
@@ -3,41 +3,61 @@ import torch.nn as nn
 
 from torchvision import transforms
 from torchvision.models import (
-    efficientnet_v2_s,
-    EfficientNet_V2_S_Weights
+    efficientnet_v2_s
 )
 
 from PIL import Image
 
+# =====================================================
+# IMAGE TRANSFORM
+# =====================================================
+
 transform = transforms.Compose([
+
     transforms.Resize((224, 224)),
+
     transforms.ToTensor(),
+
     transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225]
     )
 ])
 
+# =====================================================
+# LOAD MODEL
+# =====================================================
+
 def load_model(idx_to_class):
 
     model = efficientnet_v2_s(weights=None)
 
-    model.classifier[1] = nn.Linear(
-        model.classifier[1].in_features,
-        len(idx_to_class)
-    )
+    num_classes = len(idx_to_class)
 
-    model.load_state_dict(
-        torch.load(
-            "lepto_model.pth",
-            map_location=torch.device("cpu")
+    model.classifier[1] = nn.Sequential(
+
+        nn.Dropout(0.4),
+
+        nn.Linear(
+            model.classifier[1].in_features,
+            num_classes
         )
     )
+
+    state_dict = torch.load(
+        "lepto_model.pth",
+        map_location=torch.device("cpu")
+    )
+
+    model.load_state_dict(state_dict)
 
     model.eval()
 
     return model
 
+# =====================================================
+# PREDICT
+# =====================================================
 
 def predict(model, image_path, idx_to_class):
 
